fc5baf7254be602554982d5a768822bf8306e02f
BeanWei <157318439@qq.com>
2018-11-26 14:44:37 +0800

    🎷 BWOnlinePlayer

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 ## 💌
 ⚠ 正在施工中···
 
+## 🌞 Screenshots
+<div>
+    <img src='./Screenshots/homelist.png' width=300>
+    <img src='./Screenshots/playlist.png' width=300>
+    <img src='./Screenshots/searchview.png' width=300>
+</div>
+
 ## LICENSE
 >此项目仅为学习Go web开发以及Gin框架的练手之作，请勿用作商业用途，请勿通过本项目下载盗版歌曲资源，否则后果自负！
 
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/BeanWei/BWOnlineMusicPlayer/common"
 	"github.com/BeanWei/BWOnlineMusicPlayer/controllers"
 	"github.com/gin-gonic/gin"
 )
@@ -8,11 +9,19 @@ import (
 func main() {
 	router := gin.New()
 
+	// Gin原生中间件
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
-	router.LoadHTMLGlob("templates/*")
+	// 自定义的中间件
+	router.Use(common.CORSMiddleware())
 
+	// 静态文件
+	router.LoadHTMLFiles("templates/index.html")
+	router.Static("/static", "./static")
+	router.Static("/images", "./static/images")
+
+	// 请求
 	router.GET("/player", func(c *gin.Context) {
 		c.HTML(200, "index.html", nil)
 	})
